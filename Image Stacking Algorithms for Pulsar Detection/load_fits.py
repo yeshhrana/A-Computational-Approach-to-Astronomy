5ceec68e4b9cb238a5b3c34b2657ca0c245409c2
Yash Rana <76732393+yeshhrana@users.noreply.github.com>
2021-11-03 22:51:07 +0530

    Update load_fits.py

--- Image Stacking Algorithms for Pulsar Detection/load_fits.py
+++ Image Stacking Algorithms for Pulsar Detection/load_fits.py
@@ -1,10 +1,11 @@
 from astropy.io import fits
 import numpy as np
+
 def load_fits(x):
-    hdulist = fits.open(x)
-    data = hdulist[0].data
+  hdulist = fits.open(x)
+  data = hdulist[0].data
     
-    arg_max = np.argmax(data)
-    pos_max = np.unravel_index(arg_max, data.shape)
+  arg_max = np.argmax(data)
+  pos_max = np.unravel_index(arg_max, data.shape)
     
-    return pos_max
+  return pos_max
